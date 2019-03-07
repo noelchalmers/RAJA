1d1e150b4739a7813982cbb766b9f3d983f5083e
Noel Chalmers <noel.chalmers@gmail.com>
2019-03-07 10:45:40 -0600

    Fixed bug in ValueLoc reducer causing incorrect results in hip-reduce-loc test

--- include/RAJA/pattern/detail/reduce.hpp
+++ include/RAJA/pattern/detail/reduce.hpp
@@ -100,10 +100,10 @@ public:
   T val = doing_min ? operators::limits<T>::max() : operators::limits<T>::min();
   Index_type loc = -1;
 
-  constexpr ValueLoc() = default;
-  constexpr ValueLoc(ValueLoc const &) = default;
+  RAJA_HOST_DEVICE constexpr ValueLoc() = default;
+  RAJA_HOST_DEVICE constexpr ValueLoc(ValueLoc const &) = default;
 
-  ValueLoc &operator=(ValueLoc const &) = default;
+  RAJA_HOST_DEVICE ValueLoc &operator=(ValueLoc const &) = default;
 
   RAJA_HOST_DEVICE constexpr ValueLoc(T const &val) : val{val}, loc{-1} {}
   RAJA_HOST_DEVICE constexpr ValueLoc(T const &val, Index_type const &loc)
